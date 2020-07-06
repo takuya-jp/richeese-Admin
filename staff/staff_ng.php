7649e61d07e487e7aa10593aa26508cf4a0f882a
takuya-jp <togawa61@gmail.com>
2020-07-06 14:48:51 +0900

    スタッフ選択のエラー画面完成

--- staff/staff_ng.php
+++ staff/staff_ng.php
@@ -1,27 +1,29 @@
 <?php
 session_start();
 session_regenerate_id(true);
+
+define('TITLE', 'スタッフ選択エラー');
+
 if (isset($_SESSION['login']) === false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  header('Location: /richeese-Admin/login/staff_login.php');
   exit();
 } else {
-  print $_SESSION['staff_name'];
-  print 'さんログイン中<br>';
-  print '<br>';
-
+  $staff_name = $_SESSION['staff_name'];
 }
-?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>スタッフを選択して下さい</title>
-</head>
-<body>
-  スタッフが選択されていません<br>
-  <a href="staff_list.php">戻る</a>
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/richeese-Admin/assets/_inc/head.php');
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/richeese-Admin/assets/_inc/header.php');
+
+?>
+<main class="main">
+  <div class="section-container">
+    <section class="result">
+      <h1 class="level1-heading">スタッフ選択エラー</h1>
+      <p class="result-icon result-icon--error"><i class="fas fa-times"></i></p>
+      <p class="result-message">スタッフが選択されていません</p>
+      <div class="result-btn"><a class="btn btn--small btn--transparent btn--link_transparent" href="/richeese-Admin/staff/staff_list.php">戻る</a></div>
+    </section>
+  </div>
+</main>
 </body>
 </html>
