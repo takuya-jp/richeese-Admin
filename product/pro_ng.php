b3bf11cdeaa1eae0aa2f778fc463ed4cd52c6f9a
takuya-jp <togawa61@gmail.com>
2020-07-07 15:41:53 +0900

    商品選択NGページ完成

--- product/pro_ng.php
+++ product/pro_ng.php
@@ -1,27 +1,30 @@
-<?php
+<<?php
 session_start();
 session_regenerate_id(true);
+
+define('TITLE', '商品選択エラー');
+
 if (isset($_SESSION['login']) === false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  header('Location: /richeese-Admin/login/staff_login.php');
   exit();
 } else {
-  print $_SESSION['staff_name'];
-  print 'さんログイン中<br>';
-  print '<br>';
-
+  $staff_name = $_SESSION['staff_name'];
 }
-?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>商品を選択して下さい</title>
-</head>
-<body>
-  商品が選択されていません<br>
-  <a href="pro_list.php">戻る</a>
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/richeese-Admin/assets/_inc/head.php');
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/richeese-Admin/assets/_inc/header.php');
+
+?>
+<main class="main">
+  <div class="section-container">
+    <section class="staff-select-error">
+      <h1 class="level1-heading level1-heading--margin-top_none">商品選択エラー</h1>
+      <p class="login-name login-name__border_bottom"><?= $staff_name; ?>さん ログイン中</p>
+      <p class="result-icon result-icon--error"><i class="fas fa-times"></i></p>
+      <p class="result-message">商品が選択されていません</p>
+      <div class="result-btn"><a class="btn btn--small btn--transparent btn--link_transparent" href="/richeese-Admin/product/pro_list.php">戻る</a></div>
+    </section>
+  </div>
+</main>
 </body>
 </html>
