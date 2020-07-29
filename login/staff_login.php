0102c2d8246541c5930d99f5378fbfe6e1bfcb6c
takuya-jp <togawa61@gmail.com>
2020-07-29 09:17:22 +0900

    ログイン画面画像パス変更

--- login/staff_login.php
+++ login/staff_login.php
@@ -6,8 +6,8 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . '/richeese-Admin/assets/_inc/head.php'
 <main class="main">
   <div class="section-container">
     <section class="login">
-      <h1><img src="../assets/img/logo@2x.png" alt="画像：リッチーズ画像"></h1>
-      <form class="login-form" method="post" action="staff_login_check.php">
+      <h1><img src="/richeese-Admin/assets/img/logo@2x.png" alt="画像：リッチーズ画像"></h1>
+      <form class="login-form" method="post" action="/richeese-Admin/login/staff_login_check.php">
 
         <div class="text-box">
           <label class="text-box__label" for="code">スタッフコード</label>
